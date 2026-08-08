3114c7d98122f98dd745f2062e66ddacbb9855a3
pooja-cybersecurity01 <poojamathapati49@gmail.com>
2026-08-08 18:59:47 +0530

    added password checker project

---
+++ password_checker.py
@@ -0,0 +1,38 @@
+import string
+from getpass import getpass
+password= getpass("Enter your password: ")
+
+password = input("Enter your password: ")
+
+score = 0
+
+if len(password) >= 8:
+    score += 1
+
+if any(char.isupper() for char in password):
+    score += 1
+
+if any(char.islower() for char in password):
+    score += 1
+
+if any(char.isdigit() for char in password):
+    score += 1
+
+if any(char in string.punctuation for char in password):
+    score += 1
+
+if score <= 2:
+    print("Password Strength: Weak")
+elif score <= 4:
+    print("Password Strength: Medium")
+else:
+    print("Password Strength: Strong")
+password= getpass("Enter your password: ")
+print("\nPassword Requirements:")
+print("- At least 8 characters")
+print("- One uppercase letter")
+print("- One lowercase letter")
+print("- One number")
+print("- One special character")
+
+
